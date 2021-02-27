be57ddd90b7fbc3051f8d83d02525c1fcc539141
Matt Hill <matt.hill.email@gmail.com>
2021-02-27 09:25:53 -0800

    asdf

--- src/ServerScriptService/Door/Door.lua
+++ src/ServerScriptService/Door/Door.lua
@@ -47,7 +47,6 @@ function module.initDoor(props)
     local parentFolder = props.parentFolder
     local keyName = props.keyName
     local doorWidth = props.doorWidth or 8
-    -- local width = 20
     local width = props.width or 32
     local noGem = props.noGem
 
