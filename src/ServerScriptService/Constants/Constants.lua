24e2a5abd48b5d4ef153e6303d625cc37d7a20be
Matt Hill <matt.hill.email@gmail.com>
2021-02-28 02:24:52 -0800

    add troll spinner

--- src/ServerScriptService/Constants/Constants.lua
+++ src/ServerScriptService/Constants/Constants.lua
@@ -21,8 +21,8 @@ if isDev then
     activeSpawn = 'Spawn_Center'
 else
     activeSpawn = 'SpawnLocation_L3_ramp'
-    activeSpawn = 'SpawnLocation_L3H1'
     activeSpawn = 'Spawn_Center'
+    activeSpawn = 'SpawnLocation_L3H1'
 end
 
 local devGameConfig = {
