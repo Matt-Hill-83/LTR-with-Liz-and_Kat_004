67ed668ba2100f8902f4f305f859445918b89067
Matt Hill <matt.hill.email@gmail.com>
2021-03-02 00:37:55 -0800

    sdf

--- src/ServerScriptService/LevelConfigs/Level4.lua
+++ src/ServerScriptService/LevelConfigs/Level4.lua
@@ -1,19 +1,64 @@
+local Sss = game:GetService('ServerScriptService')
+
 local module = {}
 
-local config = {
+local hexIslandConfigs = {
     {
-        words = {
-            "SAT", --
-            "RAT", --
-            "CAT", --
-            "HAT", --
-            "MAT", --
-            "PAT", --
-            "VAT" --
+        hexNum = 1,
+        statueConfigs = {},
+        bridgeConfigs = {
+            {item = nil},
+            {
+                item = 'Rink',
+                itemConfig = {
+                    grabbers = {'CAT'}
+                }
+            },
+            {item = nil}
+        }
+    },
+    {
+        hexNum = 2,
+        statueConfigs = {},
+        bridgeConfigs = {
+            {item = nil},
+            {
+                item = 'Rink',
+                itemConfig = {
+                    grabbers = {'HAT'}
+                }
+            },
+            {item = nil}
+        }
+    },
+    {
+        hexNum = 3,
+        statueConfigs = {},
+        bridgeConfigs = {
+            {item = nil},
+            {
+                item = 'Rink',
+                itemConfig = {
+                    grabbers = {'RAT'}
+                }
+            },
+            {item = nil}
         }
     }
 }
 
-module.sectorConfigs = config
+module.teleporter = 'xxx'
+module.vendingMachines = {{targetWordIndex = 1}}
+module.hexIslandConfigs = hexIslandConfigs
+
+function module.getTargetWords()
+    return {
+        {
+            {word = 'CAT', target = 1, found = 0},
+            {word = 'HAT', target = 1, found = 0},
+            {word = 'RAT', target = 1, found = 0}
+        }
+    }
+end
 
 return module
