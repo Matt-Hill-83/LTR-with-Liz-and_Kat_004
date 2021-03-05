a6da28148667cc1c73414a1ff2b889e1767d6924
Matt Hill <matt.hill.email@gmail.com>
2021-03-05 08:16:51 -0800

    level8

---
+++ src/ServerScriptService/LevelConfigs/Level8.lua
@@ -0,0 +1,41 @@
+local module = {}
+
+local hexIslandConfigs = {
+    {
+        hexNum = 1,
+        material = Enum.Material.Grass,
+        statueConfigs = {},
+        bridgeConfigs = {
+            {
+                item = 'Rink2',
+                itemConfig = {
+                    grabbers = {'DOG', 'LOG'}
+                },
+                material = Enum.Material.LeafyGrass
+            },
+            {
+                item = 'Rink2',
+                itemConfig = {
+                    grabbers = {'HOG', 'BOG'}
+                },
+                material = Enum.Material.LeafyGrass
+            }
+        }
+    }
+}
+
+module.vendingMachines = {{targetWordIndex = 1}}
+module.hexIslandConfigs = hexIslandConfigs
+
+function module.getTargetWords()
+    return {
+        {
+            {word = 'DOG', target = 1, found = 0},
+            {word = 'LOG', target = 1, found = 0},
+            {word = 'HOG', target = 1, found = 0},
+            {word = 'BOG', target = 1, found = 0}
+        }
+    }
+end
+
+return module
--- src/ServerScriptService/LevelConfigs/LevelConfigs.lua
+++ src/ServerScriptService/LevelConfigs/LevelConfigs.lua
@@ -6,6 +6,7 @@ local Level4 = require(Sss.Source.LevelConfigs.Level4)
 local Level5 = require(Sss.Source.LevelConfigs.Level5)
 local Level6 = require(Sss.Source.LevelConfigs.Level6)
 local Level7 = require(Sss.Source.LevelConfigs.Level7)
+local Level8 = require(Sss.Source.LevelConfigs.Level8)
 
 local module = {}
 
@@ -16,18 +17,20 @@ module.levelConfigs = {
     Level4,
     Level5,
     Level6,
-    Level7
+    Level7,
+    Level8
 }
 
 module.levelDefs = {
     -- {num = 'Liz-and-Kat', name = 'pink planets', id = '6468893018'},
     {num = 'LK-LTR-1', name = 'ramps', id = '6358192824'},
+    {num = 'LK-LTR-7', name = '7', id = '6477631350'},
     -- {num = 'LK-LTR-2', name = '2', id = '6460817067'},
     {num = 'LK-LTR-3', name = '3', id = '6461486490'},
     {num = 'LK-LTR-4', name = 'caveWorld', id = '6467713882'},
     {num = 'LK-LTR-5', name = 'END', id = '6468118694'},
-    {num = 'LK-LTR-6', name = 'END', id = '6468893018'},
-    {num = 'LK-LTR-7', name = '7', id = '6477631350'}
+    {num = 'LK-LTR-8', name = 'END', id = '6358192824'},
+    {num = 'LK-LTR-6', name = 'END', id = '6468893018'}
     -- {num = 'LK-LTR-Main', name = 'Start', id = '6473099511'},
 }
 local unUsed = {
