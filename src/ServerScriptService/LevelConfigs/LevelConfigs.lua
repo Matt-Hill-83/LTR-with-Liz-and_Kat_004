4f4a1eb7a06f09f2d982b6b21dec216fbac2b1da
Matt Hill <matt.hill.email@gmail.com>
2021-03-05 08:41:33 -0800

    LV9

--- src/ServerScriptService/LevelConfigs/LevelConfigs.lua
+++ src/ServerScriptService/LevelConfigs/LevelConfigs.lua
@@ -7,6 +7,7 @@ local Level5 = require(Sss.Source.LevelConfigs.Level5)
 local Level6 = require(Sss.Source.LevelConfigs.Level6)
 local Level7 = require(Sss.Source.LevelConfigs.Level7)
 local Level8 = require(Sss.Source.LevelConfigs.Level8)
+local Level009 = require(Sss.Source.LevelConfigs.Level009)
 
 local module = {}
 
@@ -18,25 +19,23 @@ module.levelConfigs = {
     Level5,
     Level6,
     Level7,
-    Level8
+    Level8,
+    Level009
 }
 
 module.levelDefs = {
     -- {num = 'Liz-and-Kat', name = 'pink planets', id = '6468893018'},
     {num = 'LK-LTR-1', name = 'ramps', id = '6358192824'},
+    {num = 'LK-LTR-009', name = 'END', id = '6478277568'},
     {num = 'LK-LTR-7', name = '7', id = '6477631350'},
     -- {num = 'LK-LTR-2', name = '2', id = '6460817067'},
     {num = 'LK-LTR-3', name = '3', id = '6461486490'},
     {num = 'LK-LTR-4', name = 'caveWorld', id = '6467713882'},
     {num = 'LK-LTR-5', name = 'END', id = '6468118694'},
-    {num = 'LK-LTR-8', name = 'END', id = '6477887663'},
-    {num = 'LK-LTR-6', name = 'END', id = '6468893018'}
+    {num = 'LK-LTR-6', name = 'END', id = '6468893018'},
+    {num = 'LK-LTR-8', name = 'END', id = '6477887663'}
     -- {num = 'LK-LTR-Main', name = 'Start', id = '6473099511'},
 }
-local unUsed = {
-    -- {num = 'LK-LTR-1', name = 'ramps', id = '6358192824'},
-    {num = 'LK-LTR-8', name = '8', id = 'xxx'},
-    {num = 'LK-LTR-9', name = '9', id = 'xxx'}
-}
+local unUsed = {}
 
 return module
