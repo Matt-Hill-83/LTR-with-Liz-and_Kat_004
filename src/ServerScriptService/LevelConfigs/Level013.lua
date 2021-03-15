ce3f674bec3c15365d9bc3effd94e5160c70b556
Matt Hill <matt.hill.email@gmail.com>
2021-03-15 05:19:23 -0700

    asdf

--- src/ServerScriptService/LevelConfigs/Level013.lua
+++ src/ServerScriptService/LevelConfigs/Level013.lua
@@ -33,14 +33,7 @@ local blank = {
     orbiterConfigs = {}
 }
 
-local r2c1 = {
-    -- material = Enum.Material.Air,
-    material = Enum.Material.Glacier,
-    statueConfigs = {},
-    bridgeConfigs = {},
-    orbiterConfigs = {}
-}
-local r2c3 = {
+local c1r1 = {
     material = Enum.Material.Glacier,
     statueConfigs = {},
     bridgeConfigs = {
@@ -76,7 +69,7 @@ local r2c3 = {
             angularVelocity = 0.2,
             -- diameter = 32,
             discTransparency = 0.7,
-            collideDisc = true,
+            collideDisc = false,
             collideBlock = false,
             singleWord = 'C',
             discHeight = 1
@@ -111,14 +104,31 @@ local r2c3 = {
             -- discTransparency = 0.7,
             collideDisc = false,
             collideBlock = false,
-            singleWord = 'A'
+            singleWord = 'H'
         }
     }
 }
-local r1c4 = {
+
+local c1r2 = {
     material = Enum.Material.Glacier,
     statueConfigs = {},
     bridgeConfigs = {
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
         {
             item = 'Rink',
             itemConfig = {}
@@ -135,27 +145,11 @@ local r1c4 = {
             angularVelocity = 0.2,
             -- diameter = 32,
             discTransparency = 0.7,
-            collideDisc = true,
+            collideDisc = false,
             collideBlock = false,
-            singleWord = 'S',
+            singleWord = 'A',
             discHeight = 1
-        }
-    }
-}
-local r3c4 = {
-    material = Enum.Material.Glacier,
-    statueConfigs = {},
-    bridgeConfigs = {
-        {
-            item = 'Rink',
-            itemConfig = {}
         },
-        {
-            item = 'Rink',
-            itemConfig = {}
-        }
-    },
-    orbiterConfigs = {
         {
             -- words = {'CAT', 'CAT', 'CAT'},
             numBlocks = 12,
@@ -166,14 +160,51 @@ local r3c4 = {
             collideBlock = false,
             singleWord = 'T',
             discHeight = 1
+        },
+        {
+            -- words = {'CAT', 'CAT', 'CAT'},
+            numBlocks = 12,
+            angularVelocity = 0.2,
+            -- diameter = 32,
+            discTransparency = 0.7,
+            collideDisc = true,
+            collideBlock = false,
+            singleWord = 'R',
+            discHeight = 1
+        },
+        {
+            -- words = {'CAT', 'CAT', 'CAT'},
+            numBlocks = 12,
+            angularVelocity = -0.4,
+            -- diameter = 32,
+            -- discTransparency = 0.7,
+            collideDisc = false,
+            collideBlock = false,
+            singleWord = 'H'
         }
     }
 }
 
-local r2c5 = {
+local c2r2 = {
     material = Enum.Material.Glacier,
     statueConfigs = {},
     bridgeConfigs = {
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
+        {
+            item = 'Rink',
+            itemConfig = {}
+        },
         {
             item = 'Rink',
             itemConfig = {}
@@ -185,9 +216,42 @@ local r2c5 = {
     },
     orbiterConfigs = {
         {
+            words = {'CAT', 'CAT', 'CAT'},
+            numBlocks = 12,
+            angularVelocity = 0.4,
+            -- diameter = 32,
+            discTransparency = 0.7,
+            collideDisc = false,
+            collideBlock = false,
+            -- singleWord = 'A',
+            discHeight = 1
+        },
+        {
             -- words = {'CAT', 'CAT', 'CAT'},
             numBlocks = 12,
-            angularVelocity = -0.2,
+            angularVelocity = 0.2,
+            -- diameter = 32,
+            discTransparency = 0.7,
+            collideDisc = true,
+            collideBlock = false,
+            singleWord = 'T',
+            discHeight = 1
+        },
+        {
+            -- words = {'CAT', 'CAT', 'CAT'},
+            numBlocks = 12,
+            angularVelocity = 0.2,
+            -- diameter = 32,
+            discTransparency = 0.7,
+            collideDisc = true,
+            collideBlock = false,
+            singleWord = 'R',
+            discHeight = 1
+        },
+        {
+            -- words = {'CAT', 'CAT', 'CAT'},
+            numBlocks = 12,
+            angularVelocity = -0.4,
             -- diameter = 32,
             -- discTransparency = 0.7,
             collideDisc = false,
@@ -197,22 +261,19 @@ local r2c5 = {
     }
 }
 
-local r2c7 = blank
-local r2c9 = blank
-local r2c9a = blank
-local r2c9b = blank
+local c0r0 = blank
 
 local hexIslandConfigs = {
-    blank,
-    r1c4,
-    r2c1,
-    r2c3,
-    r2c5,
-    r2c7,
-    r2c9,
-    r2c9a,
-    r2c9b,
-    r3c4
+    c0r0,
+    c1r1,
+    c1r2,
+    c2r2,
+    c2r2,
+    c2r2,
+    c2r2,
+    c2r2,
+    c2r2,
+    c2r2
 }
 
 module.vendingMachines = {{targetWordIndex = 1}}
@@ -220,16 +281,16 @@ module.hexIslandConfigs = hexIslandConfigs
 
 function module.getTargetWords()
     return {
-        -- {
-        --     {word = 'CAT', target = 1, found = 0}
-        -- }
         {
-            {word = 'CAT', target = 3, found = 0},
-            {word = 'BAT', target = 3, found = 0},
-            {word = 'RAT', target = 3, found = 0},
-            -- {word = 'PAT', target = 3, found = 0},
-            {word = 'HAT', target = 3, found = 0}
+            {word = 'CAT', target = 1, found = 0}
         }
+        -- {
+        --     {word = 'CAT', target = 1, found = 0},
+        --     {word = 'BAT', target = 1, found = 0},
+        --     {word = 'RAT', target = 1, found = 0},
+        --     -- {word = 'PAT', target = 1, found = 0},
+        --     {word = 'HAT', target = 1, found = 0}
+        -- }
         -- {
         --     {word = 'DOG', target = 1, found = 0},
         --     {word = 'LOG', target = 1, found = 0},
