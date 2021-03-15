6f0abdf55e291ac7eaa010f67ca8fade32675dfa
Matt Hill <matt.hill.email@gmail.com>
2021-03-15 14:45:35 -0700

    asdf

--- src/ServerScriptService/LevelConfigs/LevelConfigs.lua
+++ src/ServerScriptService/LevelConfigs/LevelConfigs.lua
@@ -36,7 +36,7 @@ module.levelConfigs = {
 module.levelDefs = {
     -- Keep this one so we can access the config
     {num = 'LK-LTR-Main', name = 'start', id = '6358192824'},
-    {num = 'LK-LTR-4', name = 'caveWorld', id = '6467713882'},
+    -- {num = 'LK-LTR-4', name = 'caveWorld', id = '6467713882'},
     {num = 'LK-LTR-5', name = 'END', id = '6468118694'},
     {num = 'LK-LTR-6', name = 'END', id = '6468893018'},
     {num = 'LK-LTR-7', name = '7', id = '6477631350'},
