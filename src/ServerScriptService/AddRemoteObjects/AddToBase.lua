6e68b347e674393745f4dc673917fa1d05a9a9ae
Matt Hill <matt.hill.email@gmail.com>
2021-03-21 06:41:17 -0700

    asdf

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -72,7 +72,7 @@ end
 function module.addConveyors(level, sectorConfigs)
     local islandTemplate = Utils.getFromTemplates('IslandTemplate')
 
-    module.initAnimalSounds()
+    -- module.initAnimalSounds()
     module.initAnimalSounds2()
 
     local islandPositioners = Utils.getByTagInParent({parent = level, tag = 'IslandPositioner'})
