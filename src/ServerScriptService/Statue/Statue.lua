2057ae0f423b07cfb99dec82ebe1b372e3e58f39
Matt Hill <matt.hill.email@gmail.com>
2021-04-02 04:22:59 -0700

    comment out old files

--- src/ServerScriptService/Statue/Statue.lua
+++ src/ServerScriptService/Statue/Statue.lua
@@ -2,7 +2,7 @@ local Sss = game:GetService('ServerScriptService')
 local CS = game:GetService('CollectionService')
 
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
-local InitWord = require(Sss.Source.WordWheelIsland.InitWord)
+local InitWord = require(Sss.Source.Statue.InitWord)
 local LetterGem = require(Sss.Source.LetterGem.LetterGem)
 local LetterUtils = require(Sss.Source.Utils.U004LetterUtils)
 local GemUtils = require(Sss.Source.Utils.U006LetterGemUtils)
--- src/ServerScriptService/xxxWordWheelIsland/LetterWheelIsland.lua
+++ src/ServerScriptService/xxxWordWheelIsland/LetterWheelIsland.lua
@@ -1,29 +1,27 @@
 -- local CS = game:GetService("CollectionService")
-local Sss = game:GetService("ServerScriptService")
-local RS = game:GetService("ReplicatedStorage")
+local Sss = game:GetService('ServerScriptService')
+local RS = game:GetService('ReplicatedStorage')
 
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local HandleClick = require(Sss.Source.LetterFall.HandleClick)
 local LetterFallUtils = require(Sss.Source.LetterFall.LetterFallUtils)
 local DropBox = require(Sss.Source.DropBox.DropBox)
 
-local letterFallFreezeCameraRE = RS:WaitForChild("LetterFallFreezeCameraRE")
+local letterFallFreezeCameraRE = RS:WaitForChild('LetterFallFreezeCameraRE')
 
 local module = {}
 
 function configCouchTrigger(miniGameState)
     local letterFallFolder = miniGameState.letterFallFolder
 
-    local seats = Utils.getDescendantsByName(letterFallFolder, "LFCouchSeat")
+    local seats = Utils.getDescendantsByName(letterFallFolder, 'LFCouchSeat')
 
     for i, seat in ipairs(seats) do
         local seatTriggerEnabled = false
         local currentPlayer = nil
         function initGame()
-            local cameraPath1 = Utils.getFirstDescendantByName(letterFallFolder,
-                                                               "ScreenCameraPath1")
-            local cameraPath2 = Utils.getFirstDescendantByName(letterFallFolder,
-                                                               "ScreenCameraPath2")
+            local cameraPath1 = Utils.getFirstDescendantByName(letterFallFolder, 'ScreenCameraPath1')
+            local cameraPath2 = Utils.getFirstDescendantByName(letterFallFolder, 'ScreenCameraPath2')
 
             local humanoid = seat.Occupant
             if humanoid then
@@ -33,16 +31,14 @@ function configCouchTrigger(miniGameState)
                         seatTriggerEnabled = true
                     end
                     currentPlayer = player
-                    letterFallFreezeCameraRE:FireClient(player, cameraPath1,
-                                                        cameraPath2, true)
+                    letterFallFreezeCameraRE:FireClient(player, cameraPath1, cameraPath2, true)
                     return
                 end
             end
 
             -- player leaves seat
             if currentPlayer then
-                letterFallFreezeCameraRE:FireClient(currentPlayer, cameraPath1,
-                                                    cameraPath2, false)
+                letterFallFreezeCameraRE:FireClient(currentPlayer, cameraPath1, cameraPath2, false)
                 currentPlayer = nil
             end
         end
@@ -55,36 +51,35 @@ function configCouchTrigger(miniGameState)
             return initGame
         end
 
-        seat:GetPropertyChangedSignal("Occupant"):Connect(
-            initGameWrapper(miniGameState))
+        seat:GetPropertyChangedSignal('Occupant'):Connect(initGameWrapper(miniGameState))
     end
 end
 
 function initGameToggle(miniGameState)
     local letterFallFolder = miniGameState.letterFallFolder
-    local startGameTrigger = Utils.getFirstDescendantByName(letterFallFolder,
-                                                            "StartGameTrigger")
+    local startGameTrigger = Utils.getFirstDescendantByName(letterFallFolder, 'StartGameTrigger')
 
     function onCorrectItemDropped()
-        local manHoleCover = Utils.getFirstDescendantByName(letterFallFolder,
-                                                            "ManHoleCover")
-        if manHoleCover then manHoleCover:Destroy() end
+        local manHoleCover = Utils.getFirstDescendantByName(letterFallFolder, 'ManHoleCover')
+        if manHoleCover then
+            manHoleCover:Destroy()
+        end
     end
 
-    local dropBox = Utils.getFirstDescendantByName(letterFallFolder,
-                                                   "DropBoxModel")
-    local dropBoxItem = {name = "GemTemplate"}
-    DropBox.configDropBox({
-        scene = letterFallFolder,
-        sceneIndex = 0,
-        dropBox = dropBox,
-        onCorrectItemDropped = onCorrectItemDropped,
-        item = dropBoxItem
-
-    })
+    local dropBox = Utils.getFirstDescendantByName(letterFallFolder, 'DropBoxModel')
+    local dropBoxItem = {name = 'GemTemplate'}
+    DropBox.configDropBox(
+        {
+            scene = letterFallFolder,
+            sceneIndex = 0,
+            dropBox = dropBox,
+            onCorrectItemDropped = onCorrectItemDropped,
+            item = dropBoxItem
+        }
+    )
 
     function onPartTouched(otherPart)
-        local humanoid = otherPart.Parent:FindFirstChildWhichIsA("Humanoid")
+        local humanoid = otherPart.Parent:FindFirstChildWhichIsA('Humanoid')
         if humanoid then
             if not miniGameState.initCompleted then
                 miniGameState.initCompleted = true
