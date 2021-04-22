8ce8742f82e309a50a1f76273ed447f43ec835e0
Matt Hill <matt.hill.email@gmail.com>
2021-04-22 04:29:31 -0700

    publish to Troll Read Good

--- src/ServerScriptService/Constants/Constants.lua
+++ src/ServerScriptService/Constants/Constants.lua
@@ -32,15 +32,16 @@ if isDev then
     activeSpawn = 'Spawn_Snowflake_3'
     activeSpawn = 'Spawn_Snowflake_6'
     activeSpawn = 'Spawn_Snowflake_7'
-    activeSpawn = 'Spawn_Snowflake_8'
     activeSpawn = 'Spawn_Snowflake_9'
     activeSpawn = 'Spawn_Snowflake_4'
+    activeSpawn = 'Spawn_Snowflake_8'
 else
     activeSpawn = 'Spawn_Center'
     activeSpawn = 'Spawn_Start'
     activeSpawn = 'Spawn_Snowflake'
     activeSpawn = 'Spawn_Snowflake_3'
     activeSpawn = 'Spawn_Snowflake_4'
+    activeSpawn = 'Spawn_Snowflake_8'
 end
 
 local devGameConfig = {
