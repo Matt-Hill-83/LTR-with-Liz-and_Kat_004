e0d820dbbcfa04908dd8aedff559e9b98220a229
Matt Hill <matt.hill.email@gmail.com>
2021-04-26 04:45:25 -0700

    asdf

--- src/ServerScriptService/leaderstats.lua
+++ src/ServerScriptService/leaderstats.lua
@@ -1,8 +1,16 @@
--- game.Players.PlayerAdded:Connect(function(player)
---     local leaderstats = Instance.new("Folder")
---     leaderstats.Name = "leaderstats"
---     leaderstats.Parent = player
---     local Wins = Instance.new("IntValue")
---     Wins.Name = "Wins"
---     Wins.Parent = leaderstats
--- end)
+game.Players.PlayerAdded:Connect(
+    function(player)
+        local leaderstats = Instance.new('Folder')
+        leaderstats.Name = 'leaderstats'
+        leaderstats.Parent = player
+
+        local Wins = Instance.new('IntValue')
+        Wins.Name = 'Wins'
+        Wins.Parent = leaderstats
+
+        local money = Instance.new('IntValue')
+        money.Name = 'Money'
+        money.Value = 5000
+        money.Parent = leaderstats
+    end
+)
