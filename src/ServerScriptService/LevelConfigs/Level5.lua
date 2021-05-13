4dcca5af56ab3e9c8bdaabe41a8d3988841bcd82
Matt Hill <matt.hill.email@gmail.com>
2021-05-13 10:33:12 -0700

    asdf

--- src/ServerScriptService/LevelConfigs/Level5.lua
+++ src/ServerScriptService/LevelConfigs/Level5.lua
@@ -1,9 +1,19 @@
 local Sss = game:GetService('ServerScriptService')
 
 local Configs = require(Sss.Source.Constants.Const_08_Configs)
+local Words = require(Sss.Source.Constants.Const_07_Words)
 local module = {}
 
 local tallWalls = Configs.tallWalls
+
+-- 'words01',
+-- 'words02',
+-- 'words03',
+-- 'words04',
+-- 'words05',
+-- 'words06',
+-- 'words07',
+-- 'words08',
 
 local r007 = {
     bridgeConfigs = {
@@ -19,9 +29,6 @@ local r008 = {
     hexGearConfigs = {
         {
             words = {
-                '',
-                '',
-                '',
                 'FOX',
                 'FOX',
                 'FOX'
@@ -55,7 +62,72 @@ local regions = {
     r012 = r008
 }
 
+function module.getRegionTemplate(props)
+    local hexGearWords = props.hexGearWords
+    local strayRegionWords = props.strayRegionWords
+    local targetWords = props.targetWords
+    local statueConfigs = props.targetWords
+
+    local regionTemplate = {
+        hexGearConfigs = hexGearWords,
+        bridgeConfigs = {
+            invisiWallProps = tallWalls,
+            straysOnBridges = false,
+            bridgeTemplateName = Configs.bridges.default
+        },
+        strayRegions = {{words = strayRegionWords}},
+        statueConfigs = statueConfigs,
+        getTargetWords = function()
+            return {targetWords}
+        end
+    }
+
+    return regionTemplate
+end
+
+local statueConfigs = {
+    Liz = {
+        sentence = {'I', 'SEE', 'A', 'CAT'},
+        character = 'lizHappy'
+        -- songId = '6342102168',
+    },
+    Kat = {
+        sentence = {'NOT', 'A', 'CAT'},
+        character = 'katScared',
+        songId = '6342102168'
+    },
+    Troll = {
+        sentence = {'TROLL', 'NEED', 'GOLD'},
+        character = 'babyTroll04',
+        songId = '6338745550'
+    }
+}
+
+local numRegions = 50
+for i = 10, numRegions do
+    local mod = i % #statueConfigs
+
+    print('mod' .. ' - start')
+    print(mod)
+
+    local statueConfig = statueConfigs[mod + 1]
+
+    local regionName = 'r0' .. i
+
+    local props = {
+        hexGearWords = {{words = {'CAT', 'BAT', 'HAT'}}},
+        strayRegionWords = {'MAT'},
+        targetWords = {{word = 'HAT', target = 1, found = 0}},
+        statueConfigs = {statueConfig}
+    }
+    local region = module.getRegionTemplate(props)
+
+    regions[regionName] = region
+end
+
+print('regions' .. ' - start')
+print(regions)
+module.regions = regions
 Configs.addDefaults(regions)
 
-module.regions = regions
 return module
