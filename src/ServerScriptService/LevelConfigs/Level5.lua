a05f1f5043d739b912a8b707f5f9ecbda05047a9
Matt Hill <matt.hill.email@gmail.com>
2021-05-28 08:25:02 -0700

    works!!

--- src/ServerScriptService/LevelConfigs/Level5.lua
+++ src/ServerScriptService/LevelConfigs/Level5.lua
@@ -8,7 +8,8 @@ local module = {}
 local LetterUtils = require(Sss.Source.Utils.U004LetterUtils)
 local tallWalls = Configs.tallWalls
 
-local numStatuesPerHex = 18
+local numStatuesPerHex = 9
+-- local numStatuesPerHex = 18
 local numHexes = 3
 
 local hexGearConfigs = {}
